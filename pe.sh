cc3c892a2b003ecf40396fd57379e656c077319d
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2021-07-28 13:26:22 +0530

    Update pe.sh

--- pe.sh
+++ pe.sh
@@ -1,86 +1,28 @@
 #!/usr/bin/env bash
 
-echo "Updating..."
-sudo apt-get update && sudo apt-get upgrade -y
-echo"Downloading platformtools"
-wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-echo "Unziping platformtools"
-unzip platform-tools-latest-linux.zip -d platform-tools
-sudo apt install git
-echo "Downloading configuration script"
-git clone https://github.com/akhilnarang/scripts
-echo "CD"
-cd scripts
-echo "Running configuration script"
-./setup/android_build_env.sh
-echo "CD"
-cd /workspace/titan
-echo "Updating environment"
-echo "# add Android SDK platform tools to path" >> ~/.profile
-echo "if [ -d "$HOME/platform-tools" ] ; then" >> ~/.profile
-echo "    PATH="$HOME/platform-tools:$PATH"" >> ~/.profile
-echo "fi" >> ~/.profile
-echo "# set PATH so it includes user's private bin if it exists" >> ~/.profile
-echo "if [ -d "$HOME/bin" ] ; then" >> ~/.profile
-echo "    PATH="$HOME/bin:$PATH"" >> ~/.profile
-echo "fi" >> ~/.profile
-source ~/.profile
-echo "export CCACHE_DIR=/workspace/titan/.ccache" >> ~/.bashrc
-echo "export USE_CCACHE=1" >> ~/.bashrc
-echo "export CCACHE_EXEC=$(command -v ccache)" >> ~/.bashrc
-echo "ccache -M 50G" >> ~/.bashrc
-echo "export USE_NINJA=false" >> ~/.bashrc
-source ~/.bashrc
-source ~/.profile
-echo "ccache"
-export CCACHE_DIR=/workspace/titan/.ccache
-export USE_CCACHE=1
-echo "ccache"
-export CCACHE_EXEC=$(command -v ccache)
-echo "ccache"
-ccache -M 50G
-export USE_NINJA=false
-echo "Updating environment"
-source ~/.bashrc
-echo "Updating environment"
-source ~/.profile
-echo "Creating bin"
-mkdir -p bin
 echo "Creating android/pe"
-mkdir -p android/pe
-echo "Downloading repo"
-curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-echo "Installing repo"
-chmod a+x ~/bin/repo
-echo "Updating environment"
-source ~/.profile
-echo "Set git email"
-git config --global user.email "you@example.com"
-echo "Set git name"
-git config --global user.name "Your Name"
-echo "Updating..."
-sudo apt-get update && sudo apt-get upgrade -y
-echo "CD"
-cd android/pe
+mkdir -p ~/android/pe
+cd ~/android/pe
 echo "Initializing the PE source repository"
 repo init -q --no-repo-verify --depth=1 -u https://github.com/PixelExperience/manifest -b ten-plus
 echo "Downloading the source code"
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j30 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
 echo "Cloning device tree"
-git clone https://github.com/thedeadfish59/android_device_motorola_titan --depth 1 -b ten device/motorola/titan
+git clone --depth 1 https://github.com/thedeadfish59/android_device_motorola_titan -b ten device/motorola/titan
 echo "Cloning device kernel"
-git clone https://github.com/thedeadfish59/android_kernel_motorola_msm8226 --depth 1 -b ten kernel/motorola/msm8226
+git clone --depth 1 https://github.com/thedeadfish59/android_kernel_motorola_msm8226 -b ten kernel/motorola/msm8226
 echo "Cloning device vendor"
-git clone https://github.com/thedeadfish59/proprietary_vendor_motorola --depth 1 -b ten vendor/motorola
+git clone --depth 1 https://github.com/thedeadfish59/proprietary_vendor_motorola -b ten vendor/motorola
 echo "Cloning device common"
-git clone https://github.com/thedeadfish59/android_device_motorola_msm8226-common -b ten device/motorola/msm8226-common
+git clone --depth 1 https://github.com/thedeadfish59/android_device_motorola_msm8226-common -b ten device/motorola/msm8226-common
 echo "Cloning qcom"
-git clone https://github.com/thedeadfish59/android_system_qcom -b ten system/qcom
+git clone --depth 1 https://github.com/thedeadfish59/android_system_qcom -b ten system/qcom
 echo "Preparing the device-specific code"
+cd ~/android/pe
 source build/envsetup.sh
 echo "Lunch"
 lunch aosp_titan-userdebug
 echo "Croot"
 croot
-echo "Building"
-mka bacon -j30
+echo "mka bacon -j30"
+
