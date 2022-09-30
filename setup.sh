d2c619207db7a02553e117907744655e09a7e290
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-10-01 00:33:27 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,36 +1,24 @@
 #!/usr/bin/env bash
 
-sudo apt update && sudo apt upgrade -y 
-
+sudo apt update && sudo apt upgrade -y
 sudo apt -y install wget
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg -i chrome-remote-desktop_current_amd64.deb 
 sudo dpkg --configure -a
 sudo apt --fix-broken install -y
-
-echo "Updating environment"
+mkdir rom
+curl -sL $OUTFILE https://git.io/file-transfer | sh 
+wget https://raw.githubusercontent.com/Sushrut1101/GoFile-Upload/master/upload.sh
 sudo apt-get update && sudo apt-get upgrade -y
-echo "Installing the build packages"
-sudo apt-get install -y mc bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
-echo "Downloading platformtools"
+sudo apt-get install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev python jq seahorse-nautilus nautilus firefox
 wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-echo "Unziping platformtools"
 unzip platform-tools-latest-linux.zip -d ~
-echo "Running akhil's script"
-sudo apt install -y git
 git clone https://github.com/akhilnarang/scripts
 cd scripts
 ./setup/android_build_env.sh
-echo "Creating bin"
 mkdir -p ~/bin
-echo "setting up repo"
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
-echo "Config git"
-git config --global user.email "rushiranpise17@gmail.com"
-git config --global user.name "Rushi Ranpise"
-git config --global color.ui true
-echo "Updating profile"
 echo "# add Android SDK platform tools to path" >> ~/.profile
 echo "if [ -d "$HOME/platform-tools" ] ; then" >> ~/.profile
 echo "    PATH="$HOME/platform-tools:$PATH"" >> ~/.profile
@@ -39,8 +27,10 @@ echo "# set PATH so it includes user's private bin if it exists" >> ~/.profile
 echo "if [ -d "$HOME/bin" ] ; then" >> ~/.profile
 echo "    PATH="$HOME/bin:$PATH"" >> ~/.profile
 echo "fi" >> ~/.profile
-echo "Updating environment"
-sudo apt install -y python jq
+sudo snap install notepad-plus-plus
+git config --global user.email "rushiranpise17@gmail.com"
+git config --global user.name "Rushi Ranpise"
+git config --global color.ui true
 sudo apt-get update && sudo apt-get upgrade -y
 source ~/.bashrc
 source ~/.profile
@@ -48,14 +38,4 @@ cat /etc/os*
 df -h
 free -h
 nproc
-
 echo "done"
-
-sudo snap install notepad-plus-plus
-git config --global user.email "rushiranpise17@gmail.com"
-git config --global user.name "Rushi Ranpise"
-git config --global color.ui true
-sudo apt-get install seahorse-nautilus nautilus -y && nautilus -q
-mkdir rom
-curl -sL $OUTFILE https://git.io/file-transfer | sh 
-wget https://raw.githubusercontent.com/Sushrut1101/GoFile-Upload/master/upload.sh
