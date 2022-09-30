d81c029bba7dea71aab701feaf03dd0f577a2a2c
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-10-01 00:42:24 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 
 sudo apt update && sudo apt upgrade -y
-sudo apt -y install wget
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt -y install wget bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev python jq seahorse-nautilus nautilus firefox openjdk-8-jdk libwxgtk3.0-dev libwxgtk3.0-gtk3-dev
+
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg -i chrome-remote-desktop_current_amd64.deb 
 sudo dpkg --configure -a
 sudo apt --fix-broken install -y
+
 mkdir rom
+
 curl -sL $OUTFILE https://git.io/file-transfer | sh 
 wget https://raw.githubusercontent.com/Sushrut1101/GoFile-Upload/master/upload.sh
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev python jq seahorse-nautilus nautilus firefox
+
 wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
 unzip platform-tools-latest-linux.zip -d ~
+
 git clone https://github.com/akhilnarang/scripts
 cd scripts
 ./setup/android_build_env.sh
+
 mkdir -p ~/bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
@@ -27,13 +32,19 @@ echo "# set PATH so it includes user's private bin if it exists" >> ~/.profile
 echo "if [ -d "$HOME/bin" ] ; then" >> ~/.profile
 echo "    PATH="$HOME/bin:$PATH"" >> ~/.profile
 echo "fi" >> ~/.profile
+
 sudo snap install notepad-plus-plus
+
 git config --global user.email "rushiranpise17@gmail.com"
 git config --global user.name "Rushi Ranpise"
 git config --global color.ui true
+
+sudo apt update && sudo apt upgrade -y
 sudo apt-get update && sudo apt-get upgrade -y
+
 source ~/.bashrc
 source ~/.profile
+
 cat /etc/os*
 df -h
 free -h
